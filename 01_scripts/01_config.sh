1726b8b6ccdad241c8863d8cf048b731ea7fd848
Claire Mérot <claire.merot@gmail.com>
2021-05-13 12:57:15 -0400

    update config for min depth

--- 01_scripts/01_config.sh
+++ 01_scripts/01_config.sh
@@ -2,16 +2,20 @@
 BAM_PATH=../wgs_sample_preparation/09_no_overlap
 
 #path to pcaangsd
+#this is the former version of ANGSD - it uses python2 and may require some libraries.
 PCA_ANGSD_PATH=/project/lbernatchez/programs/pcangsd 
 
 #filter : will keep SNP above this allele frequency (over all individuals)
 MIN_MAF=0.05 
 
-#filter : will keep SNP with at least one read for this percentage of individuals (over all individuals in step 03, and within each pop at step 07)
-PERCENT_IND=0.5 
+#filter : will keep positions with at least MIN_DEPTH reads for each individual, This is not necessaily for all individuals, we consider a PERCENT_IND (percentage of individuals over all individuals in step 03, and within each pop at step 07)
+#advice: as min depth use a value that is approximately what you expect or a bit below.
+#advice: as percentage, avoid going below 50% and also consider the whole number of individuals. (it may makes sense to use 50% with 100 ind/pop, but you may want 90% with 9 ind/pop
+PERCENT_IND=0.5
+MIN_DEPTH=1
 
-#filter: will keep SNP with at least a coverage of this factor multiplied by the number of ind - across all ind. usually set 2-4
-#times the coverage to remove repeated regions
+#filter: will keep SNP with at least a coverage of this factor multiplied by the number of ind - across all ind. 
+# advice: we usually set it at 2-4 times the expected coverage to remove repeated regions
 MAX_DEPTH_FACTOR=3
 
 #window size for sliding window FST & Thetas
