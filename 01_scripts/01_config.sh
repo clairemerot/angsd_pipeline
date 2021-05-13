a29d7f52cfdcc5bb4f5457b3c5142ee38f05b0c5
Claire Mérot <claire.merot@gmail.com>
2021-05-13 13:04:48 -0400

    Update 01_config.sh

--- 01_scripts/01_config.sh
+++ 01_scripts/01_config.sh
@@ -9,7 +9,7 @@ PCA_ANGSD_PATH=/project/lbernatchez/programs/pcangsd
 MIN_MAF=0.05 
 
 #filter : will keep positions with at least MIN_DEPTH reads for each individual, This is not necessaily for all individuals, we consider a PERCENT_IND (percentage of individuals over all individuals in step 03, and within each pop at step 07)
-#advice: as min depth use a value that is approximately what you expect or a bit below.
+#advice: as min depth use a value that is a bit below what you expected. we use 1 for 1X of coverage but if you have 5X it may make sense to put the bar a bit higher to 2 or 3
 #advice: as percentage, avoid going below 50% and also consider the whole number of individuals. (it may makes sense to use 50% with 100 ind/pop, but you may want 90% with 9 ind/pop
 PERCENT_IND=0.5
 MIN_DEPTH=1
