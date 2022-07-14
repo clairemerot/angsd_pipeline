12cfd5f59f604bd7e236fdfb5c5ee5c9871fb7b4
Claire Mérot <claire.merot@gmail.com>
2022-07-14 13:42:58 -0400

    Update and rename 11_plink_pruning.sh to 11_plink_pruning_using_sites.sh

--- 01_scripts/11_plink_pruning_using_sites.sh
+++ 01_scripts/11_plink_pruning_using_sites.sh
@@ -11,8 +11,7 @@
 ###this script will work on all bamfiles and output a plink file for each chromosome
 #maybe edit
 NB_CPU=4 #change accordingly in SLURM header
-REGIONS="-rf 02_info/regions_25kb_100snp.txt" 
-#REGIONS="-rf 02_info/chromosomes.txt" 
+
 
 # Important: Move to directory where job was submitted
 cd $SLURM_SUBMIT_DIR
@@ -24,22 +23,18 @@ ulimit -S -n 2048
 #prepare variables - avoid to modify
 source 01_scripts/01_config.sh
 
-N_IND=$(wc -l 02_info/bam.filelist | cut -d " " -f 1)
-MIN_IND_FLOAT=$(echo "($N_IND * $PERCENT_IND)"| bc -l)
-MIN_IND=${MIN_IND_FLOAT%.*} 
-MAX_DEPTH=$(echo "($N_IND * $MAX_DEPTH_FACTOR)" |bc -l)
 
 echo " output genotype in plink format for all individuals listed in 02_info/bam.filelist"
-echo "keep loci with at leat one read for n individuals = $MIN_IND, which is $PERCENT_IND % of total $N_IND individuals"
-echo "filter on allele frequency = $MIN_MAF"
-
+echo "work on SNPs listed in the sites file"
 
 ####Calculate the SAF, MAF and GL
 
-angsd -P $NB_CPU -nQueueSize 50 -doPlink 2 -doMaf 1 -doCounts 1 \
--GL 2 -doMajorMinor 1 -doGeno -4 -doPost 1 -postCutoff 0.8 \
--anc 02_info/genome.fasta -remove_bads 1 -minMapQ 30 -minQ 20 $REGIONS \
--minInd $MIN_IND -minMaf $MIN_MAF -setMaxDepth $MAX_DEPTH -b 02_info/bam.filelist \
+angsd -P $NB_CPU -doPlink 2 -doMaf 1 -doCounts 1 \
+-GL 2 -doMajorMinor 3 -doGeno -4 -doPost 1 -postCutoff 0.8 \
+-anc 02_info/genome.fasta -remove_bads 1 -minMapQ 30 -minQ 20 \
+-sites 02_info/sites_all_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR" \
+-rf 02_info/regions_all_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR" \
+-b 02_info/bam.filelist \
 -out 11_plink/all_plink_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR"
 
 echo "provide a list of LD-pruned snp"
